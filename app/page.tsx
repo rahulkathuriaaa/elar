7d327fba833f43c9c05827321bf1736cf5833463
rahulkathuriaaa <rahulkathuria008@gmail.com>
2025-03-11 00:42:10 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { NavBar } from "@/components/tubelight-navbar";
+import { HeroSectionDemo } from "@/UI/demo";
+
+
+export default function Home() {
+
+
+
+  return (
+   <div>
+    <NavBar/>
+    <HeroSectionDemo/>
+
+   </div>
+  );
+}
